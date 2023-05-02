3238529cf9e798d28817d39c0e14b86191e177a7
strictlysimple <bojanmitreskii@gmail.com>
2023-05-02 21:59:55 +0200

    Added first dequeuereusable cell

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -11,6 +11,19 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
     
 }
