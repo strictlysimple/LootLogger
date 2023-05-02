bbd89da0ebbc60083517cb2564038db2b34b9070
strictlysimple <bojanmitreskii@gmail.com>
2023-05-02 18:35:43 +0200

    itemStore injection in SceneDeleagate, p: 192

---
+++ LootLogger/ItemStore.swift
@@ -0,0 +1,26 @@
+//
+//  ItemStore.swift
+//  LootLogger
+//
+//  Created by Bojan Mitreski on 2.5.23.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+}
---
+++ LootLogger/ItemsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Bojan Mitreski on 2.5.23.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+    
+    
+}
