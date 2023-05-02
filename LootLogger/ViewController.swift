6e3f49f1d70fdb4e6bd78f7ef4c890a180a503db
strictlysimple <bojanmitreskii@gmail.com>
2023-05-02 15:31:19 +0200

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Bojan Mitreski on 2.5.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
