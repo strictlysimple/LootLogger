99b7fa93f125ad48ce3f93025a07036b9fc1daa8
strictlysimple <bojanmitreskii@gmail.com>
2023-05-09 13:11:02 +0200

    Chapter 11 start, p: 225

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -64,9 +64,13 @@ class ItemsViewController: UITableViewController {
         
         let item = itemStore.allItems[indexPath.row]
         
+        
         cell.nameLabel.text = item.name
         cell.serialNumberLabel.text = item.serialNumber
         cell.valueLabel.text = "$\(item.valueInDollars)"
+            if item.valueInDollars >= 50 {
+            cell.valueLabel.textColor = UIColor.systemGreen
+        }
         
         return cell
     }
